37fb6baf0937617461a81cec0642df1cfdb1f6ea
Kaleb Harris <kalebjharris2016@gmail.com>
2019-06-24 09:08:55 -0400

    Portfolio Site

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+function enlargeSchool() {
+  document.getElementById('school').style.width="75%";
+}
+
+function enlargeClubs() {
+  document.getElementById('robotics').style.width="100%";
+}
+
+function enlargePets() {
+  document.getElementById('dogs').style.width="100%";
+}
+
+function enlargeSite() {
+  document.getElementById('site').style.width="100%";
+}
+
+function shrinkSchool() {
+  document.getElementById('school').style.width="50%";
+}
+
+function shrinkClubs() {
+  document.getElementById('robotics').style.width="75%";
+}
+
+function shrinkPets() {
+  document.getElementById('dogs').style.width="75%";
+}
+
+function shrinkSite() {
+  document.getElementById('site').style.width="75%";
+}
